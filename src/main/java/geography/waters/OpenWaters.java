9481fc45bafc0bd3cf6c37ef85d178c358f233b4
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-07-03 21:29:58 +0300

    refactoring

--- src/main/java/geography/waters/OpenWaters.java
+++ src/main/java/geography/waters/OpenWaters.java
@@ -12,21 +12,11 @@ public class OpenWaters {
         oceanList = new ArrayList<>();
 
         //Data from: https://en.wikipedia.org/wiki/Ocean#Oceanic_divisions (cited: 03.07.2022 20:08 GMT+2)
-        oceanList.add(new Ocean(
-                "Pacific",
-                168.723 * Math.pow(10, 12)));
-        oceanList.add(new Ocean(
-                "Atlantic",
-                85.133 * Math.pow(10, 12)));
-        oceanList.add(new Ocean(
-                "Indian",
-                70.560 * Math.pow(10, 12)));
-        oceanList.add(new Ocean(
-                "Southern",
-                21.960 * Math.pow(10, 12)));
-        oceanList.add(new Ocean(
-                "Arctic",
-                15.558 * Math.pow(10, 12)));
+        oceanList.add(new Ocean("Pacific", 168.723));
+        oceanList.add(new Ocean("Atlantic", 85.133));
+        oceanList.add(new Ocean("Indian", 70.560));
+        oceanList.add(new Ocean("Southern", 21.960));
+        oceanList.add(new Ocean("Arctic", 15.558));
 
         setOceanAreaSum();
     }
@@ -34,23 +24,13 @@ public class OpenWaters {
     private static void setOceanAreaSum() {
         double areaSum = 0;
         for (Ocean ocean : oceanList) {
-            areaSum += ocean.area;
+            areaSum += ocean.getArea();
         }
         oceanAreaSum = areaSum;
     }
 
     public static double totalOceanArea() {
         return oceanAreaSum;
-    }
-
-    static class Ocean {
-        String name;
-        double area;
-
-        Ocean(String name, double area) {
-            this.name = name;
-            this.area = area;
-        }
     }
 
     //for abstraction
