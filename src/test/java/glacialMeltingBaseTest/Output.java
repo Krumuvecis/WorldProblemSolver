de088f9c89bc4ecfbb69f58f24c5ef9fece3f327
Oskars AA <66279792+Krumuvecis@users.noreply.github.com>
2022-07-03 20:22:56 +0300

    output improvements

--- src/test/java/glacialMeltingBaseTest/Output.java
+++ src/test/java/glacialMeltingBaseTest/Output.java
@@ -1,5 +1,6 @@
 package glacialMeltingBaseTest;
 
+import geography.waters.OpenWaters;
 import glacialMelting.GlacialMelting;
 import glacialMelting.YearlyData;
 
@@ -17,12 +18,31 @@ class Output {
      * @param glacialMelting A GlacialMelting object.
      */
     protected static void output(@NotNull GlacialMelting glacialMelting) {
+        printInitialParameters();
+        printYearlyData(glacialMelting);
+    }
+
+    private static void printInitialParameters() {
+        printLine("Initial parameters:");
+        printLine("Total ocean area: "
+                + doubleToString(OpenWaters.totalOceanArea() / Math.pow(10, 12), 0)
+                + " millions of km2");
+    }
+
+    private static void printYearlyData(@NotNull GlacialMelting glacialMelting) {
         for (YearlyData year : glacialMelting.yearList) {
             separator();
-            printLine("year: " + year.year
-                    + ", global temperature: +" + doubleToString(year.temperature, 1) + " C");
-            printLine("glacier volume: " + doubleToString(year.glacierVolume / Math.pow(10, 12), 1) + " thousands of km3");
-            printLine("sea level rise: " + doubleToString(year.seaLevelRise, 3) + " m");
+            printLine("Year: "
+                    + year.year
+                    + ", global temperature: +"
+                    + doubleToString(year.temperature, 1)
+                    + " C");
+            printLine("glacier volume: "
+                    + doubleToString(year.glacierVolume / Math.pow(10, 12), 1)
+                    + " thousands of km3");
+            printLine("sea level rise: "
+                    + doubleToString(year.seaLevelRise * 100, 2)
+                    + " cm");
         }
     }
 }
